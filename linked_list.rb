8d524fbe1203514391970c9019e95a11d6b4a0c8
Michael Duke <michaelcduke@gmail.com>
2016-03-11 14:45:44 -0500

    Create Node and SinglyLinkedList class.

---
+++ linked_list.rb
@@ -0,0 +1,15 @@
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize(value)
+    @value = value
+    @next_node = nil
+  end
+
+end
+
+class SinglyLinkedList
+  def initialize(head)
+    @head = head
+  end
+end
