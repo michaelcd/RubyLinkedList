318f6ada4b19bbc73599c2e7b1a1a79624632751
Michael Duke <michaelcduke@gmail.com>
2016-03-11 15:29:37 -0500

    Complete methods for both classes.

--- linked_list.rb
+++ linked_list.rb
@@ -1,9 +1,29 @@
 class Node
-  attr_accessor :value, :next_node
+  attr_accessor :value, :prev, :next
 
   def initialize(value)
     @value = value
-    @next_node = nil
+    @prev = nil
+    @next = nil
+  end
+
+  def next
+    @next
+  end
+
+  def prev
+    @prev
+  end
+
+  def insert(node)
+    node.prev = self
+    @next = node
+    node
+  end
+
+  def remove
+    self.prev.next = nil
+    self.prev = nil
   end
 
 end
@@ -11,5 +31,26 @@ end
 class SinglyLinkedList
   def initialize(head)
     @head = head
+    @last = head
+  end
+
+  def first
+    @head
+  end
+
+  def last
+    @last
+  end
+
+  def push(node)
+    @last.insert(node)
+    @last = node
+  end
+
+  def pop
+    popped = @last
+    @last = @last.prev
+    popped.remove
+    popped
   end
 end
